572595a61dac167781d17087c0cd872d952c2fb4
Chetan Malviya <chetanmalviya230@gmail.com>
2025-08-27 14:39:50 +0530

    Pushing the Edusity

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <>
+    <div className='footer'>
+      <p>© 2024 Edusity. All rights reserved.</p>
+      <ul>
+        <li>Terms of Services</li>
+        <li>Privacy Policy</li>
+      </ul>
+    </div>
+    <div className="footer-credit">
+        Designed and built with ❤️ by Chetan Malviya
+      </div>
+    </>
+  )
+}
+
+export default Footer
